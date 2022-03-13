cd3b113e79bef5a0141049322dd9df4cab85c513
JohnKarima <karimajohn24@gmail.com>
2022-03-13 03:26:22 -0700

    Correcting smile in th emiror

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,27 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 /**
- * *main -  checks if the random n number is positive or negative
- * *Return: 0
- * */
+ *main -  print the alphabet (reversed)
+ *Return: 0
+ */
 int main(void)
 {
-	int n;
+	int i;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if (n > 0)
+	for (i = 'z'; i >= 'a'; i--)
 	{
-		printf("%d is positive\n", n);
+		putchar(i);
 	}
-	else if (n == 0)
-	{
-		printf("%d is zero\n", n);
-	}
-	else
-	{
-		printf("%d is negative\n", n);
-	}
+	putchar('\n');
 	return (0);
 }

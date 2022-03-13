bae6d66bbf94a1a37245c38c3765f57e5e885f0c
JohnKarima <karimajohn24@gmail.com>
2022-03-13 01:42:49 -0800

    printing combinations

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main -  print the comibnation of single-digit numbers
+ *Return: 0
+ */
+int main(void)
+{
+	int i;
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+		if (i == '9')
+		{
+			continue;
+		}
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}

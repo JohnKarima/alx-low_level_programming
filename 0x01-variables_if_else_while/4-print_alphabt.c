5ecd764e1498a8de2667b100fd2a4508fec1df33
JohnKarima <karimajohn24@gmail.com>
2022-03-13 01:35:56 -0800

    More alphabet stuff

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main -  print the alphabet (lowercase except e and q)
+ *Return: 0
+ */
+int main(void)
+{
+	int i;
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (i == 'q' || i == 'e')
+		{
+			continue;
+		}
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}

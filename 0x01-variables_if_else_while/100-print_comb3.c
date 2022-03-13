8a4b73bc4d7c1ac824a3faaac36e5e9f2c9ca740
JohnKarima <karimajohn24@gmail.com>
2022-03-13 01:46:26 -0800

    Bonus attempts

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+include <stdio.h>
+/**
+ *main -  print a comibnation of two-digit numbers
+ *Return: 0
+ */
+int main(void)
+{
+	int i, j;
+	for (i = 0; i < 9; i++)
+	{
+		for (j = i + 1; j < 10; j++)
+		{
+			putchar(i % 10 + '0');
+			putchar(j % 10 + '0');
+			if (i == 8 && j == 9)
+			{
+				break;
+			}
+			else
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ *main -  print a comibnation of two two-digit numbers
+ *Return: 0
+ */
+int main(void)
+{
+	int i, j;
+	for (i = 0; i <= 98; i++)
+	{
+		for (j = i + 1; j <= 99; j++)
+		{
+			putchar(i / 10 + '0');
+			putchar(i % 10 + '0');
+			putchar(32);
+			putchar(j / 10 + '0');
+			putchar(j % 10 + '0');
+			if (i / 10 != 9 || i % 10 != 8)
+			{
+				putchar(44);
+				putchar(32);
+			}
+		}
+	}
+	putchar(10);
+	return (0);
+}
